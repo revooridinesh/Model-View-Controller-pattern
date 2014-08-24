7de3b20cb0c05503f5a8e25a78001928e701832d
revooridinesh <revooridinesh@gmail.com>
2014-08-24 15:30:41 -0400

    First Commit - Main Window Layout

---
+++ ModelViewController_Project/ModelViewController_Project/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	ModelViewController_Project::MyForm form;
+	Application::Run(%form);
+}
---
+++ ModelViewController_Project/ModelViewController_Project/MyForm.h
@@ -0,0 +1,81 @@
+#pragma once
+
+namespace ModelViewController_Project {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(127, 40);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(183, 20);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Model View Controller";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(428, 365);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"Model View Controller";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
